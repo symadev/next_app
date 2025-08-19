5d0b789b74101e49a5a7f2145902675efd08c361
syma sultana <symasultana2002@gmail.com>
2025-08-20 01:54:15 +0600

    added examples

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,13 +1,16 @@
 
-"use client";
+
 // throw new Error('there is an error in the mission page')
+
+import Button from "@/app/component/button";
 
 export default function missionPage() {
   return (
     <div>
     <div>This is my main mission page </div>
+    <Button></Button>
     
-          <button onClick={()=> console.log("i have to click that button ")} className='px-4 py-2 bg-green-600'>click here</button>
+        
           </div>
   )
 }
---
+++ app/component/button.js
@@ -0,0 +1,7 @@
+"use client";
+
+export default function Button() {
+  return (
+     <button onClick={()=> console.log("i have to click that button ")} className='px-4 py-2 bg-green-600'>click here</button>
+  )
+}
