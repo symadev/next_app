d9c1e76a9b07fb2eea66e5e9b6c6a1cbe121a995
syma sultana <symasultana2002@gmail.com>
2025-08-20 10:59:35 +0600

    aadded images

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,13 +1,17 @@
 
-
+"use client";
 // throw new Error('there is an error in the mission page')
 
 import Button from "@/app/component/button";
+import Image from "next/image";
+
+
 
 export default function missionPage() {
   return (
     <div>
     <div>This is my main mission page </div>
+     <Image src="/images/image.png" alt="Mission Image" width={500} height={300} />
     <Button></Button>
     
         
