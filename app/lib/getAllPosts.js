8fa49a79e43ff58362a37837cb472eb2464eb4d8
syma sultana <symasultana2002@gmail.com>
2025-08-20 14:00:24 +0600

    added chnages

--- app/lib/getAllPosts.js
+++ app/lib/getAllPosts.js
@@ -2,6 +2,12 @@
 
 export default async function getAllPosts() {
     const result = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
+    {
+        next: { revalidate: 10 } // This will cache the response for 10 seconds
+    }
+    if (!result.ok) {
+        throw new Error('Failed to fetch posts');
+    }
                      
 
     return  result.json();//we eturn here the promise
